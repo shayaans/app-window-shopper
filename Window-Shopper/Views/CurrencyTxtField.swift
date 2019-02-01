2e50430b26d48c42880065e800620b7f11c56e8a
Shayaan Siddiqui <shayaan.siddiqui@mac.com>
2019-02-01 00:54:27 -0500

    iPad Layout Done

---
+++ Window-Shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,26 @@
+//
+//  CurrencyTxtField.swift
+//  Window-Shopper
+//
+//  Created by Shayaan Siddiqui on 2/1/19.
+//  Copyright © 2019 OneOrangeTree LLC. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+        
+        
+    }
+}
