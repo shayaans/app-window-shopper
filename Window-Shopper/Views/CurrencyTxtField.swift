19ff08c9abbfaba7a24b46cd4f01b07ecc6b76e9
Shayaan Siddiqui <shayaan.siddiqui@mac.com>
2019-02-07 00:28:08 -0500

    Added Currency Label

--- Window-Shopper/Views/CurrencyTxtField.swift
+++ Window-Shopper/Views/CurrencyTxtField.swift
@@ -12,7 +12,22 @@ import UIKit
 class CurrencyTxtField: UITextField {
     override func prepareForInterfaceBuilder() {
         customizeView()
-    }    
+    }
+    
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 0.7985605736)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -21,6 +36,7 @@ class CurrencyTxtField: UITextField {
     
     func customizeView()
     {
+        clipsToBounds = true
         backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
